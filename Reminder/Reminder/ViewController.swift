7bfa41fe266ecbca5b865887c044a27cda88ed4c
Phonthep Aungkanukulwit <mos11134@gmail.com>
2020-03-15 23:13:36 +0700

    landing: create edit button

--- Reminder/Reminder/ViewController.swift
+++ Reminder/Reminder/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func editMyLists(_ sender: Any) {
+    print("edit")
+  }
+  
 }
 
