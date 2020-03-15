cf1e3cdc33566e947017f5cc1a5af80b02967610
Phonthep Aungkanukulwit <mos11134@gmail.com>
2020-03-15 22:46:33 +0700

    initial Project

---
+++ Reminder/Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Phonthep Aungkanukulwit on 15/3/2563 BE.
+//  Copyright © 2563 Phonthep Aungkanukulwit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
