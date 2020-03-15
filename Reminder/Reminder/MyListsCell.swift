2bdbcf48a52074c090e101dca6a3c07dc60c329c
Phonthep Aungkanukulwit <mos11134@gmail.com>
2020-03-15 23:51:54 +0700

    landing: create UI layout

---
+++ Reminder/Reminder/MyListsCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyListsCell.swift
+//  Reminder
+//
+//  Created by Phonthep Aungkanukulwit on 15/3/2563 BE.
+//  Copyright © 2563 Phonthep Aungkanukulwit. All rights reserved.
+//
+
+import UIKit
+
+class MyListsCell: UITableViewCell {
+}
